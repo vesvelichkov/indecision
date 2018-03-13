bc7155745448c0c6890eb00f316760cf29698fbd
Veselin Velichkov <veselin.velichkov@komfo.com>
2018-03-13 11:50:48 +0200

    03 Manual data binding

--- src/app.js
+++ src/app.js
@@ -14,27 +14,33 @@ const template = (
   </div>
 );
 
-const user = {
-  name: 'Veselin Velichkov',
-  age: 27,
-  location: 'New York'
+let count = 0;
+const addOne = () => {
+  count++;
+  renderCounterApp();
 };
-
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  }
-}
-
-const templateTwo = (
-  <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
-  </div>
-);
+const minusOne = () => {
+  count--;
+  renderCounterApp();
+};
+const reset = () => {
+  count = 0;
+  renderCounterApp();
+};
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
-// ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);  
+};
+
+renderCounterApp();
