60e3ccbe4cb1be831ed35a44759d8f99e6cbe973
Veselin Velichkov <veselin.velichkov@komfo.com>
2018-03-19 20:56:38 +0200

    ES6 Classes Part 1 [completed]

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,10 +1,18 @@
 
 class Person {
-  constructor(name) {
+  constructor(name = 'Anonymous', age = 0) {
     this.name = name;
+    this.age = age;
+  }
+
+  getDescription() {
+    return `${this.name} is ${this.age} years(s) old.`
   }
 }
 
-const me = new Person('Ves Velichkov');
+const me = new Person('Ves Velichkov', 28);
 
-console.log(me);
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getDescription());
