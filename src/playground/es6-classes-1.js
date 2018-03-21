c8816b03fee8a58f69cc0b7a1c659a65b8fd489c
Veselin Velichkov <veselin.velichkov@komfo.com>
2018-03-21 12:05:35 +0200

    ES6 Classes Part 2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -6,13 +6,59 @@ class Person {
   }
 
   getDescription() {
-    return `${this.name} is ${this.age} years(s) old.`
+    return `${this.name} is ${this.age} years(s) old.`;
+  }
+
+  getGreeting() {
+    return `Hi. I am ${this.name}!`;
   }
 }
 
-const me = new Person('Ves Velichkov', 28);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
 
-console.log(me.getDescription());
+  hasMajor() {
+    return !!this.major;
+  }
 
-const other = new Person();
-console.log(other.getDescription());
+  getDescription() {
+    let desctiption = super.getDescription();
+
+    if (this.hasMajor()){
+      desctiption += ` Their major is ${this.major}`;
+    }
+
+    return desctiption;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  hasHomeLocation() {
+    return !!this.homeLocation;
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.hasHomeLocation()) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`;
+    }
+
+    return greeting;
+  }
+}
+
+const me = new Traveler('Veselin Velichkov', 28, 'Sofia, Bulgaria');
+
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, undefined);
+console.log(other.getGreeting());
