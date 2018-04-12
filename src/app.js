1edc31339a54d68127412721a6592220116896fb
Veselin Velichkov <veselin.velichkov@komfo.com>
2018-04-12 10:16:25 +0300

    [06] ES6 import / export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log(`Hey, app.js is now runing!`);
+// import './utils.js';
+// import { square, add } from './utils.js';
+
+// console.log(`Hey, app.js is now runing!`);
+// console.log(square(5));
+// console.log(add(5,5));
+
+// exercise
+import { isAdult, canDrink } from './person.js';
+console.log(isAdult(18));
+console.log(canDrink(21));
---
+++ src/person.js
@@ -0,0 +1,6 @@
+// console.log(`person.js is running`);
+
+const isAdult = (age) => age >= 18;
+const canDrink = (age) => age >= 21;
+
+export { isAdult, canDrink}
